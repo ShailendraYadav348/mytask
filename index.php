ba7ecd8114c4fa1a05703730ce68e5363a494ea6
ShailendraYadav348 <75174483+ShailendraYadav348@users.noreply.github.com>
2021-06-24 17:05:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,32 +5,9 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<script>
-function validateform()
-{  
-var mobile=document.form1.mobile.value;  
-if (mobile.length==10 && !isNaN(mobile))
-{  
-  if(mobile.startsWith("9")||mobile.startsWith("8")||mobile.startsWith("7"))
-  {
-    return true; 
-  }
-  else
-  {
-    alert("Mobile number is not valid");
-    return false;
-  }
-  
-  }
-  else
-  {
-    alert("Must be atleast 10 digit or Numeric");
-    return false;
-  }
-}  
-</script>
+
   <h1>Contact Book</h1>
-  <form action="data.php" method="post" name="form1" onsubmit="return validateform()">
+  <form action="data.php" method="post" name="form1">
   	Name:<input type="text" name="name"><br>
   	Mobile:<input type="text" name="mobile" id="mobile"><br>
   	Email:<input type="text" name="email"><br>
